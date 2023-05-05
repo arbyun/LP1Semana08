1d6e6bb7d08abfa84413e7018d94736de32c25cb
arbyun <someonealiveh@gmail.com>
2023-05-05 10:40:19 +0100

    add class Unit

---
+++ GameUnits/Unit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GameUnits
+{
+    public abstract class Unit
+    {
+
+        private int movement;
+        public int Health { get; set; }
+
+        public virtual void Move(int distance)
+        {
+            Console.WriteLine($"Unit moved {distance} tiles.");
+        }
+
+        //Unit constructor
+        public Unit(int movement, int health)
+        {
+            this.movement = movement;
+            Health = health;
+        }
+    }
+}
