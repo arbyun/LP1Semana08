b2a0193f983a6580df3e3faf430b2994c0b01c8c
arbyun <someonealiveh@gmail.com>
2023-05-05 11:33:07 +0100

    create method GetSecretInfo

--- GameUnits/SpyUnit.cs
+++ GameUnits/SpyUnit.cs
@@ -1,7 +1,21 @@
 ﻿namespace GameUnits
 {
-    public class SpyUnit
+    public abstract class SpyUnit : XPUnit
     {
-        
+        public override float Cost => 12.5f;
+
+        public SpyUnit() : base(8, 2)
+        {
+        }
+
+        public void GetSecretInfo(Unit unit)
+        {
+            Xp += unit switch
+            {
+                SpyUnit _ => 3,
+                MilitaryUnit _ => 2,
+                _ => 1
+            };
+        }
     }
 }
