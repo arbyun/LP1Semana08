6d94cf11e80fd94d67fb52e1657e4c1697afb1e0
arbyun <someonealiveh@gmail.com>
2023-05-05 11:02:39 +0100

    add and test military unit

--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -9,6 +9,10 @@ namespace GameUnits
             SettlerUnit settler = new SettlerUnit(1, 2);
             settler.Move(3);
             Console.WriteLine($"Settler Health: {settler.Health}, Cost: {settler.Cost}");
+            
+            MilitaryUnit soldier = new MilitaryUnit(2, 10, 5);
+            soldier.Move(5);
+            Console.WriteLine($"Soldier Health: {soldier.Health}, Cost: {soldier.Cost}");
         }
     }
 }
