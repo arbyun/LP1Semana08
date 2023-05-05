9c800fb29155e8a9cf4d671c659c8a2ed79a4560
arbyun <someonealiveh@gmail.com>
2023-05-05 10:42:27 +0100

    add class MilitaryUnit.cs

---
+++ GameUnits/MilitaryUnit.cs
@@ -0,0 +1,17 @@
+﻿namespace GameUnits
+{
+    public class MilitaryUnit: Unit
+    {
+        public MilitaryUnit(int movement, int health) : base(movement, health)
+        {
+        }
+        
+        public int AttackPower { get; }
+        public int XP { get; private set; }
+
+        public void Attack(Unit u)
+        {
+            
+        }
+    }
+}
