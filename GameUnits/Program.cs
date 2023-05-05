7e44e695f8bbbcbdbef3a27e888a087383759337
arbyun <someonealiveh@gmail.com>
2023-05-05 11:02:19 +0100

    add and test settler unit

---
+++ GameUnits/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GameUnits
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            SettlerUnit settler = new SettlerUnit(1, 2);
+            settler.Move(3);
+            Console.WriteLine($"Settler Health: {settler.Health}, Cost: {settler.Cost}");
+        }
+    }
+}
+
